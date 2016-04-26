4bf0415cc14f3e2019e590951eec0b24b4257811
FedeGaleano <fede.n.gal@hotmail.com>
2016-04-26 20:19:02 -0300

    Update nucleo.c

--- nucleo/nucleo.c
+++ nucleo/nucleo.c
@@ -78,27 +78,3 @@ int main(void)
 	
 	return EXIT_SUCCESS;
 }
-
-/*
- * leerArchivoConfig();
- * Parametros: -
- * Descripcion: Procedimiento que lee el archivo config.conf y lo carga en la variable infoConfig
- * Return: -
- */
-void leerArchivoConfig() {
-
-	t_config *config = config_create("config.conf");
-
-	if (config == NULL) {
-		free(config);
-		abort();
-	}
-	// Guardo los datos en una variable global
-	infoConfig.puerto_prog = config_get_string_value(config, "PUERTO_PROG");
-	infoConfig.puerto_cpu = config_get_string_value(config, "PUERTO_CPU");
-	infoConfig.puerto_umc = config_get_string_value(config, "PUERTO_UMC");
-
-	// No uso config_destroy(config) porque bugea
-	free(config->path);
-	free(config);
-}
