440b4a5eda5c5f40c4f9cfb5ecc34ba971fa9be2
FedeGaleano <fede.n.gal@hotmail.com>
2016-04-26 18:13:04 -0300

    Update nucleo.h

--- nucleo/nucleo.h
+++ nucleo/nucleo.h
@@ -8,6 +8,19 @@
 #ifndef NUCLEO_H_
 #define NUCLEO_H_
 
+//Bibliotecas a usar
+#include <commons/config.h>
+#include <commons/string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <unistd.h>
+
 //Cuántas conexiones se aceptarán
 #define BACKLOG 100
 //Puerto de el propio núcleo
