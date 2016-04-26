75d392164d82827ae64589585cbb5d069c78f1eb
FedeGaleano <fede.n.gal@hotmail.com>
2016-04-26 18:52:14 -0300

    Update nucleo.c

--- nucleo/nucleo.c
+++ nucleo/nucleo.c
@@ -1,21 +1,22 @@
 #include "nucleo.h"
 
-int main(void){
-
+int main(void)
+{
 	// Leer archivo config.conf
 	leerArchivoConfig();
 	
-	int fd_listener, fd_new;
-	fd_set master_fds, read_fds;
-	struct sockaddr_in miDireccion, direccionCliente;
-	int i, maxfd, yes = 1, nbytes;
-	char buffer[100];
-	miDireccion.sin_family = AF_INET;
-	miDireccion.sin_port = htons(MIPUERTO);
-	miDireccion.sin_addr.s_addr = INADDR_ANY;
-	memset(miDireccion.sin_zero, '\0', sizeof(miDireccion.sin_zero));
-	unsigned int tam = sizeof(struct sockaddr_in);
+	//file descriptor para escuchar (listener), para una nueva conexión (new) y para explorar conexiones (explorer)
+	int fd_listener, fd_new, fd_explorer;
+	
+	fd_set master_fds, read_fds;//conjuntos de fd's total(master) y para los que tienen datos para lectura(read)
+	struct direccionCliente;//direccion del cliente
+	
+	int maxfd/*cima del conjunto "master_fds" */, yes = 1/*para setsockopt()*/, nbytes/*número de bytes recibidos del cliente*/;
+	char buffer[100];//buffer para datos recibidos del cliente
+	unsigned int tam = sizeof(struct sockaddr_in);//tamaño de datos pedido por accept()
 
+	FD_ZERO(&master_fds);
+	FD_ZERO(&read_fds);
 	
 	return EXIT_SUCCESS;
 }
