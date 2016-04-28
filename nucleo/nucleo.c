e1084581f80b42a20a25dec833f02f124850ca02
FedeGaleano <fede.n.gal@hotmail.com>
2016-04-28 20:20:48 -0300

    Update nucleo.c

--- nucleo/nucleo.c
+++ nucleo/nucleo.c
@@ -1,16 +1,12 @@
 #include "nucleo.h"
 int main(void)
 {
-	// Leer archivo config.conf
 	leerArchivoConfig();
-
-	//Inicializar nuestra direccion
+	
 	inicializarDirecciones();
-
-	//Conectarse al proceso UMC
+	
 	conectar_a_umc();
 
-	//Poner en modo escucha los sockets asociados a los puertos para Consola y para CPU
 	abrirPuertos();
 
 	administrarConexiones();
