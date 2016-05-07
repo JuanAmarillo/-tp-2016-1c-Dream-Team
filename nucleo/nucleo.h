b60aaa73889d7450b2ff52082d743658fc66e55c
FedeGaleano <fede.n.gal@hotmail.com>
2016-05-06 21:17:18 -0300

    Update nucleo.h

--- nucleo/nucleo.h
+++ nucleo/nucleo.h
@@ -7,19 +7,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/time.h>
-#include <unistd.h>
-
-/*
- * Estructuras de datos
- */
-
-/*Variables Globales*/
-/*----------------------------------------------------------*/
 
 /*
  * Funciones / Procedimientos
