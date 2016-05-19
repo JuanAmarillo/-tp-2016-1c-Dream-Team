fdf02b910b0429a6007a922e44917b0521480742
PadillaJuan <juanpadilla.jip@gmail.com>
2016-05-18 21:57:20 -0300

    Agrego codigos de Mensaje

--- messageCode/messageCode.h
+++ messageCode/messageCode.h
@@ -11,11 +11,13 @@
 
 
 // DESDE LA UMC, LO QUE RECIBE EL SWAP
-#define RESERVE_SPACE 900 	// Parametros recibidos despues de la cabecera: pid(unsigned), longitud del programa(unsigned)
-#define SAVE_PROGRAM 905		// Parametros recibidos despues de la cabecera: pid(unsigned), paginas(char*)
-#define SAVE_PAGE 910			// Parametros recibidos despues de la cabecera: pid(unsigned), numeroDePagDentroDelPrograma(unsigned)
-#define END_PROGRAM 915		// Parametros recibidos despues de la cabecera: pid(unsigned)
-#define RETURN_PAGE 920	// Parametros recibidos despues de la cabecera: pid(unsigned), numeroDePagDentroDelPrograma(unsigned)
-#define NOT_ENOUGH_SPACE 930 	// Parametros recibidos despues de la cabecera: pid(unsigned)
+#define RESERVE_SPACE 900 		// Parametros recibidos despues de la cabecera: pid(unsigned), longitud del programa(unsigned)
+#define SAVE_PROGRAM 905		// Parametros recibidos despues de la cabecera: pid(unsigned), paginas(char[TAMANIO_PAGINA])
+#define SAVE_PAGE 910			// Parametros recibidos despues de la cabecera: pid(unsigned), numeroDePagDentroDelPrograma(unsigned), pagina(char[TAMANIO_PAGINA])
+#define END_PROGRAM 915			// Parametros recibidos despues de la cabecera: pid(unsigned)
+#define BRING_PAGE_TO_UMC 920	// Parametros recibidos despues de la cabecera: pid(unsigned), numeroDePagDentroDelPrograma(unsigned)
+#define NOT_ENOUGH_SPACE 925 	// Parametros recibidos despues de la cabecera: pid(unsigned)
 
+// DESDE EL SWAP, LO QUE RECIBE LA UMC
+#define SWAP_SENDS_PAGE 930		// Parametros recibidos despues de la cabecera: pagina(char[TAMANIO_PAGINA])
 #endif /* MESSAGECODE_H_ */
