162882fbc5b5da9ed190ab124ba64d5b3523d039
FedeGaleano <fede.n.gal@hotmail.com>
2016-05-20 22:26:21 -0300

    Actualizar header Planificador

--- nucleo/planificador.h
+++ nucleo/planificador.h
@@ -9,6 +9,7 @@
 #define PLANIFICADOR_H_
 
 #include <commons/collections/queue.h>
+#include "pcb.h"
 #include <stdlib.h>
 
 t_queue cola_listos;
@@ -16,5 +17,9 @@ t_queue cola_bloqueados;
 t_queue cola_salida;
 
 void roundRobin(const unsigned short int quantum, t_queue *listos, t_queue *bloqueados, t_queue *salida);
+
+int ejecutar(t_PCB proceso);
+void terminar(t_PCB proceso);
+void bloquear(t_PCB proceso);
 
 #endif /* PLANIFICADOR_H_ */
