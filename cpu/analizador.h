4eeda887d3268885faffe4d4ee8a6a530fdbf092
Marco Biondi <marco.fbb@gmail.com>
2016-06-10 15:06:24 -0300

    Parser upgrade

--- cpu/analizador.h
+++ cpu/analizador.h
@@ -11,33 +11,46 @@
 
 t_nombre_variable nombreVariable_aBuscar;
 
-t_puntero definirVariable(t_nombre_variable variable);
-t_puntero obtenerPosicionVariable(t_nombre_variable variable);
-t_valor_variable dereferenciar(t_puntero puntero);
-
 int _is_variableX(t_variable *);
-void asignar(t_puntero puntero, t_valor_variable variable);
-void imprimir(t_valor_variable valor);
-void imprimirTexto(char* texto);
-void testParser();
 
-// Configuracion del analizador
-static const int CONTENIDO_VARIABLE = 20;
-static const int POSICION_MEMORIA = 0x10;
-static const char* DEFINICION_VARIABLES = "variables a, b, c";
-static const char* ASIGNACION = "a = b + 12";
-static const char* IMPRIMIR = "print b";
-static const char* IMPRIMIR_TEXTO = "textPrint foo\n";
+t_puntero definirVariable(t_nombre_variable identificador_variable);
+t_puntero obtenerPosicionVariable(t_nombre_variable identificador_variable);
+t_valor_variable dereferenciar(t_puntero direccion_variable);
+
+void asignar(t_puntero direccion_variable, t_valor_variable valor);
+t_valor_variable obtenerValorCompartida(t_nombre_compartida variable);
+t_valor_variable asignarValorCompartida(t_nombre_compartida variable, t_valor_variable valor);
+void irAlLabel(t_nombre_etiqueta etiqueta);
+void llamarSinRetorno(t_nombre_etiqueta etiqueta);
+void llamarConRetorno(t_nombre_etiqueta etiqueta, t_puntero donde_retornar);
+void finalizar();
+void retornar(t_valor_variable retorno);
+void imprimir(t_valor_variable valor_mostrar);
+void imprimirTexto(char* texto);
+void entradaSalida(t_nombre_dispositivo dispositivo, int tiempo);
+void wait(t_nombre_semaforo identificador_semaforo);
+void signal(t_nombre_semaforo identificador_semaforo);
 
 AnSISOP_funciones functions = {
 		.AnSISOP_definirVariable		= definirVariable,
 		.AnSISOP_obtenerPosicionVariable= obtenerPosicionVariable,
 		.AnSISOP_dereferenciar			= dereferenciar,
 		.AnSISOP_asignar				= asignar,
+		.AnSISOP_obtenerValorCompartida	= obtenerValorCompartida,
+		.AnSISOP_asignarValorCompartida	= asignarValorCompartida,
+		.AnSISOP_irAlLabel				= irAlLabel,
+		.AnSISOP_llamarSinRetorno		= llamarSinRetorno,
+		.AnSISOP_llamarConRetorno		= llamarConRetorno,
+		.AnSISOP_finalizar				= finalizar,
+		.AnSISOP_retornar				= retornar,
 		.AnSISOP_imprimir				= imprimir,
 		.AnSISOP_imprimirTexto			= imprimirTexto,
+		.AnSISOP_entradaSalida			= entradaSalida,
 
 };
-AnSISOP_kernel kernel_functions = { };
+AnSISOP_kernel kernel_functions = {
+		.AnSISOP_wait					= wait,
+		.AnSISOP_signal					= signal,
+};
 
 #endif /* ANALIZADOR_H_ */
