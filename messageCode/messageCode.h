25de746b44f783b10012820f99da42bacf92ac4b
marcofbb <marco.fbb@gmail.com>
2016-06-27 16:15:01 -0300

    Codigo UMC <-> CPU <-> NUCLEO
    Codigo UMC <-> CPU <-> NUCLEO

--- messageCode/messageCode.h
+++ messageCode/messageCode.h
@@ -8,12 +8,6 @@
 #ifndef MESSAGECODE_H_
 #define MESSAGECODE_H_
 
-// CPU -> UMC
-#define GET_DATA 200 			// Parametros: pagina, offset, size
-#define GET_TAM_PAGINA 205 		// Parametros: NULL
-#define RESERVE_MEMORY 210 		// Parametros: TamañoMemoriaReservar ;
-#define RECORD_DATA 215 		// Parametros: numero_pagina, desplazamiento, tamañano, data
-
 //CONSOLA -> NUCLEO
 #define NUEVO_PROGRAMA 100
 //NUCLEO -> CONSOLA
@@ -21,23 +15,38 @@
 #define IMPRIMIR_PROGRAMA 101
 #define IMPRIMIR_TEXTO_PROGAMA 102
 
+
+// CPU -> UMC
+#define GET_TAM_PAGINA 700 
+#define GET_DATA 701
+#define RESERVE_MEMORY 702
+#define RECORD_DATA 703
+
+// UMC -> CPU
+#define RETURN_DATA 704
+#define RETURN_TAM_PAGINA 705
+#define RETURN_POS 706
+#define RETURN_RECORD_DATA 707
+
 // NUCLEO <- CPU
-#define STRUCT_PCB 300 			// Utilizar la funcion: mensaje_to_pcb() y pcb_to_mensaje() segun corresponda
-#define FIN_QUANTUM 301
-#define FIN_PROGRAMA 302
+#define STRUCT_PCB 708
+#define STRUCT_PCB_FIN 709
+#define STRUCT_PCB_FIN_ERROR 710
+#define OBTENER_COMPARTIDA 711
+#define ASIGNAR_COMPARTIDA 712
+#define IMPRIMIR 713
+#define IMPRIMIR_TEXTO 714
+#define ENTRADA_SALIDA 715
+#define WAIT 716
+#define SIGNAL 717
+
 // NUCLEO -> CPU
-#define EJECUTAR 303        //PCB para ejecutar
+#define RETURN_OBTENER_COMPARTIDA 303        //PCB para ejecutar
 #define QUANTUM 304         //Parámetros: quantum, quantumSleep(retardo de cada instruccion, usar sleep(quantumSleep);)
 
 //NUCLEO -> UMC
 #define INIT_PROG 300
 #define FIN_PROG 305
-
-// UMC -> CPU
-#define RETURN_DATA 201 		// Parametros: NULL; mensaje_extra: Bytes en memoria solicitados
-#define RETURN_TAM_PAGINA 206 	// Parametros: tamaño_pagina
-#define RETURN_POS 211 			// Parametros: numero_pagina, desplazamiento, tamaño
-#define RECORD_OK 216			// Parametros: null
 
 // DESDE LA UMC, LO QUE RECIBE EL SWAP
 #define RESERVE_SPACE 900 		// Parametros recibidos despues de la cabecera: pid(unsigned), longitud del programa(unsigned)
