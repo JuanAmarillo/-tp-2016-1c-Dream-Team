4556987ad22d4f3cdf836baaa06c4c54bfba5fba
FedeGaleano <fede.n.gal@hotmail.com>
2016-07-29 17:09:25 -0300

    Add Logs

---
+++ nucleo/archivoLog.d
@@ -0,0 +1,14 @@
+archivoLog.d: ../archivoLog.c ../archivoLog.h ../pcb.h \
+ ../metadata_program.h ../parser.h ../sintax.h ../protocolo_mensaje.h
+
+../archivoLog.h:
+
+../pcb.h:
+
+../metadata_program.h:
+
+../parser.h:
+
+../sintax.h:
+
+../protocolo_mensaje.h:
